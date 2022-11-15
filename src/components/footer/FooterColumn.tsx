e3705ec0b12c7ebfda0c2cb4010cf3db9ff647ef
d-rocham <d_rocham@outlook.com>
2022-11-15 14:40:48 -0500

    [Feature] Crea FooterColumn

---
+++ src/components/footer/FooterColumn.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FooterColumnInfo } from './FooterColumnInfo';
+
+type Props = {
+    info: FooterColumnInfo;
+};
+
+function FooterColumn({ info }: Props) {
+    return (
+        <div className="FooterColumnContainer flex flex-col text-light-text">
+            <h1 className="ColumnTitle font-semibold text-lg text-center xl:text-justify pb-6">
+                {info.title}
+            </h1>
+            <div className="ItemsContainer flex flex-col gap-4 items-center xl:items-start">
+                {info.items.map((item) => (
+                    <p className="ColumnItem font-thin">{item}</p>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default FooterColumn;
