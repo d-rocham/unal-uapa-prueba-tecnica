5d31812a33ebd6902bd8de04fae84afaf8c36182
d-rocham <d_rocham@outlook.com>
2022-11-15 14:39:32 -0500

    [Dev] Desinstala DaisyUI, mueve colores a configuracion de Tailwind, añade regla a eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	daisyui: {
-		themes: [
-			{
-				figma: {
-					primary: "#2091F9",
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        colors: {
+            primary: '#2091F9',
 
-					"secondary-green": "#2DC071",
-					"secondary-orange": "#F15B07",
+            'secondary-green': '#2DC071',
+            'secondary-orange': '#F15B07',
 
-					"text-main": "#252B42",
-					"text-secondary": "#374754",
-					"text-light": "#FFFFFF",
+            'main-text': '#252B42',
+            'secondary-text': '#374754',
+            'light-text': '#FFFFFF',
 
-					"background-dark": "#252B42",
-					"background-light": "#FFFFFF",
-					"background-gradient": "#222B32",
-				},
-			},
-		],
-	},
-	plugins: [require("daisyui"), require("@tailwindcss/forms")],
+            'background-dark': '#252B42',
+            'background-light': '#FFFFFF',
+            'background-gradient': '#222B32'
+        },
+        extend: {}
+    },
+
+    plugins: [require('@tailwindcss/forms')]
 };
