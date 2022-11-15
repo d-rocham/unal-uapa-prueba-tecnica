77908f99ea15987c7dc6b8d35d61b53e785e22a1
d-rocham <d_rocham@outlook.com>
2022-11-15 13:23:46 -0500

    [Dev] Configura el tema de DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,25 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
+	daisyui: {
+		themes: [
+			{
+				figma: {
+					primary: "#2091F9",
+
+					"secondary-green": "#2DC071",
+					"secondary-orange": "#F15B07",
+
+					"text-main": "#252B42",
+					"text-secondary": "#374754",
+					"text-light": "#FFFFFF",
+
+					"background-dark": "#252B42",
+					"background-light": "#FFFFFF",
+					"background-gradient": "#222B32",
+				},
+			},
+		],
+	},
 	plugins: [require("daisyui"), require("@tailwindcss/forms")],
 };
