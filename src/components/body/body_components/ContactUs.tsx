898c362dfc6018d8de09d163c99893def0034b85
d-rocham <d_rocham@outlook.com>
2022-11-15 20:38:00 -0500

    [Refactor] Adapta componente para dispositivos xl, renderiza condicionalmente mapa, iconos sociales

--- src/components/body/body_components/ContactUs.tsx
+++ src/components/body/body_components/ContactUs.tsx
@@ -22,24 +22,39 @@ function ContactUs() {
                 <div className="xl:hidden">
                     <SocialIcons primary />
                 </div>
-                <div className="ContactInfoIcons flex flex-col justify-start gap-4">
-                    <IconInfo
-                        icon={<MdOutlineLocationOn />}
-                        primary
-                        text="6386 Spring St Anchorage, Georgia 12473 United States"
-                    />
-                    <IconInfo
-                        icon={<MdOutlinePhoneIphone />}
-                        primary
-                        text="(843) 555-0130"
-                    />
-                    <IconInfo
-                        icon={<MdOutlineEmail />}
-                        primary
-                        text="willie.jennings@example.com"
-                    />
+                <div className="flex flex-col items-center gap-7 xl:flex-row xl:order-last">
+                    <div className="flex flex-col gap-6 justify-center items-center">
+                        <div className="ContactInfoIcons flex flex-col justify-start gap-4 xl:flex-row">
+                            <IconInfo
+                                icon={<MdOutlineLocationOn />}
+                                primary
+                                text="6386 Spring St Anchorage, Georgia 12473 United States"
+                            />
+                            <IconInfo
+                                icon={<MdOutlinePhoneIphone />}
+                                primary
+                                text="(843) 555-0130"
+                            />
+                            <IconInfo
+                                icon={<MdOutlineEmail />}
+                                primary
+                                text="willie.jennings@example.com"
+                            />
+                        </div>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.7524163956427!2d-74.0862350856184!3d4.638199143488746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9bd14943c13b%3A0x3de530667d9ff6b0!2sUniversidad%20Nacional%20de%20Colombia!5e0!3m2!1sen!2sco!4v1668561991078!5m2!1sen!2sco"
+                            width="600"
+                            height="450"
+                            loading="lazy"
+                            className="hidden xl:block xl:h-72 xl:w-96"
+                        />
+                        <div className="hidden xl:block xl:self-start">
+                            <SocialIcons primary />
+                        </div>
+                    </div>
+
+                    <ContactUsForm />
                 </div>
-                <ContactUsForm />
             </>
         </BodySection>
     );
