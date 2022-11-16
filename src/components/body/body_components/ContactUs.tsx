5e992dcfe80c63ef078c2d5c36bcf1df55ff128b
d-rocham <d_rocham@outlook.com>
2022-11-15 19:32:20 -0500

    [Feature] Añade elementos

--- src/components/body/body_components/ContactUs.tsx
+++ src/components/body/body_components/ContactUs.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { SocialIcons } from '../../utils/Utils';
+import {
+    MdOutlineLocationOn,
+    MdOutlinePhoneIphone,
+    MdOutlineEmail
+} from 'react-icons/md';
+import { SocialIcons, IconInfo } from '../../utils/Utils';
 import { BodySection, SectionDescription } from './BodyComponents';
 
 function ContactUs() {
@@ -11,6 +16,23 @@ function ContactUs() {
                     description="Most calendars are designed for teams. Slate is designed for freelancers."
                 />
                 <SocialIcons primary />
+                <div className="ContactInfoIcons flex flex-col justify-start gap-4">
+                    <IconInfo
+                        icon={<MdOutlineLocationOn />}
+                        primary
+                        text="6386 Spring St Anchorage, Georgia 12473 United States"
+                    />
+                    <IconInfo
+                        icon={<MdOutlinePhoneIphone />}
+                        primary
+                        text="(843) 555-0130"
+                    />
+                    <IconInfo
+                        icon={<MdOutlineEmail />}
+                        primary
+                        text="willie.jennings@example.com"
+                    />
+                </div>
             </>
         </BodySection>
     );
