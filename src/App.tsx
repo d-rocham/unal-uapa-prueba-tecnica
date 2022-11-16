86b7c766f0efeb6700bf94a90db57c1575934d10
d-rocham <d_rocham@outlook.com>
2022-11-15 19:24:05 -0500

    [Feature] Importa componentes a Body, añade Body a App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from './components/Components';
+import Body from './components/body/Body';
 
 function App() {
     return (
         <div className="App min-h-screen flex flex-col justify-between">
+            <Body />
             <Footer />
         </div>
     );
--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { ContactUs } from './body_components/BodyComponents';
 
 function Body() {
-    return <div>Body</div>;
+    return (
+        <div className="flex flex-col justify-center mb-12">
+            <ContactUs />
+        </div>
+    );
 }
 
 export default Body;
