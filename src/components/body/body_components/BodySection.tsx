34c5235b3190651f001772e5d6adb414e7e1b4da
d-rocham <d_rocham@outlook.com>
2022-11-15 19:32:02 -0500

    [Refactor] Corrige detalles menores en estilos del  componente

--- src/components/body/body_components/BodySection.tsx
+++ src/components/body/body_components/BodySection.tsx
@@ -4,7 +4,7 @@ type Props = {
     children: JSX.Element;
 };
 function BodySection({ children }: Props) {
-    return <div className="flex flex-col items-center gap-5">{children}</div>;
+    return <div className="flex flex-col items-center gap-7">{children}</div>;
 }
 
 export default BodySection;
