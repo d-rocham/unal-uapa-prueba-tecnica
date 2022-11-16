b7bacdd4f7cc46c9c35a4cd97863cbe9142892e4
d-rocham <d_rocham@outlook.com>
2022-11-15 23:32:57 -0500

    [Feature] Crea clase backgroundImage.hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { url } = require('inspector');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -9,7 +11,11 @@ module.exports = {
 
             'secondary-dark': '#374754'
         },
-        extend: {}
+        extend: {
+            backgroundImage: {
+                'hero-image': "url('/public/hero.jpg')"
+            }
+        }
     },
 
     plugins: [require('@tailwindcss/forms')]
