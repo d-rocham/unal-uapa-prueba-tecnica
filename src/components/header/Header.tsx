75834948f8d883150713a6c87c0542e94e1d625e
d-rocham <d_rocham@outlook.com>
2022-11-15 23:33:12 -0500

    [Feature] Implementa estilo básico para el header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 const heroImage = require('../../assets/hero.jpg');
 
 function Header() {
-    return <img src={heroImage} alt="" className="clip w-full" />;
+    return (
+        <div className="bg-hero-image bg-cover clip h-96">
+            <p>lol</p>
+        </div>
+    );
 }
 
 export default Header;
