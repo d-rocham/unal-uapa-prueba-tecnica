81e3ff6f7f686ecbc5d2474881f68f80e3ba8b09
d-rocham <d_rocham@outlook.com>
2022-11-15 23:37:09 -0500

    [Feature] Agrega boton, implementa estilos

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
-const heroImage = require('../../assets/hero.jpg');
+import { Button } from '../utils/Utils';
 
 function Header() {
     return (
-        <div className="bg-hero-image bg-cover clip h-96">
-            <p>lol</p>
+        <div className="bg-hero-image bg-cover clip flex flex-col items-center justify-center gap-7 py-44">
+            <h1 className="text-white text-center text-5xl font-bold">
+                The best products start with Figma
+            </h1>
+            <p className="text-white text-center ">
+                Most calendars are designed for teams. Slate is designed for
+                freelancers
+            </p>
+            <Button description="Try for Free" />
         </div>
     );
 }
